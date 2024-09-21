99536474c2d082394c26c340e8d347185659edd5
prakhaarr <prakhargupta965@gmail.com>
2024-09-21 14:44:33 +0530

    final update hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -25,7 +25,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            npm run dev
+            Prakhar Gupta
           </h2>
 
           <TextGenerateEffect
